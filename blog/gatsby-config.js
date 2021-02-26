1f26a8ec72dafbbba81c2ee450028a4339cba75b
Lu-Thao <thaol31@gmatc.matc.edu>
2021-02-25 19:52:51 -0600

    completed activity-5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Galaxy Blog`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `Lu Thao`,
     contact:{
